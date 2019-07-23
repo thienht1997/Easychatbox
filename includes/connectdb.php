694ae642aa4fcbc0f4a462c3637b8a287308932c
= <=>
2019-07-23 10:50:13 +0700

    .

--- includes/connectdb.php
+++ includes/connectdb.php
@@ -1,8 +1,8 @@
 <?php
-	$namehost = 'localhost';
-	$userhost = 'root';
-	$passhost = '';
-	$database = 'messeger';
+	$namehost = 'db4free.net';
+	$userhost = 'thienht1997tt';
+	$passhost = 'thien123';
+	$database = 'easychatbox';
 
 	// Lệnh kết nối tới database
 	$cn = mysqli_connect($namehost, $userhost, $passhost, $database);
